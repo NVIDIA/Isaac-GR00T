11a27d191a27cbc4eaec0e0434e5178bd9285e9d
Lance Li <lancel@nvidia.com>
2025-10-27 17:04:47 +0800

    added piper support

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1077,6 +1077,104 @@ class BinamualPandaRoboTwinDataConfig(BaseDataConfig):
 
         return ComposedModalityTransform(transforms=transforms)
     
+
+
+class SinglePiperJointSpaceDataConfig(BaseDataConfig):
+    video_keys = [
+        "video.hand_camera",
+        "video.third_camera",
+    ]
+    state_keys = [
+        "state.joint_states",
+        "state.gripper_distance",
+    ]
+    action_keys = [
+        "action.joint_states",
+        "action.gripper_distance",
+    ]
+    language_keys = ["annotation.human.action.task_description"]
+    observation_indices = [0]
+    action_indices = list(range(16))
+
+    # Used in StateActionTransform for normalization and target rotations
+    state_normalization_modes = {
+        "state.joint_states": "min_max",
+        "state.gripper_distance": "min_max",
+    }
+    action_normalization_modes = {
+        "action.joint_states": "min_max",
+        "action.gripper_distance": "min_max",
+    }
+    def modality_config(self):
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+        return modality_configs
+
+    def transform(self):
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"), # TODO: padding or FOV
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes=self.state_normalization_modes,
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes=self.action_normalization_modes,
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+            ),
+        ]
+
+        return ComposedModalityTransform(transforms=transforms)
+
+
+class SinglePiperTaskSpaceDataConfig(BaseDataConfig):
+    pass
 ###########################################################################################
 
 DATA_CONFIG_MAP = {
@@ -1095,5 +1193,7 @@ DATA_CONFIG_MAP = {
     # Galbot stack cube tasks
     "galbot_joint_space": GalbotLeftArmThreeCamerasDataConfig(),
     "galbot_abs_eef_pose": GalbotAbsEEFPoseDataConfig(),
-    "franka_robotwin": BinamualPandaRoboTwinDataConfig()
+    "franka_robotwin": BinamualPandaRoboTwinDataConfig(),
+    "piper_joint_space": SinglePiperJointSpaceDataConfig(),
+    # "piper_task_space": SinglePiperTaskSpaceDataConfig(),
 }
