a62040fd61cd760f2d9bbb0e6582f42a8adc736a
Lance Li <lancel@nvidia.com>
2026-01-14 16:59:58 +0800

    added standalone inference script

--- scripts/standalone_inference.py
+++ scripts/standalone_inference.py
@@ -16,21 +16,11 @@ policy = Gr00tPolicy(
 modality_config = policy.get_modality_config()
 print("Modality config:", modality_config)
 
-inference_rtc_frozen_steps = 4
-inference_rtc_overlap_steps = 8
-
-pridict_horizon = 16
-next_state_index = pridict_horizon - inference_rtc_overlap_steps - 1 # state should be the last state of the previous action.
-
 # 推理获取动作
 for i in range(5):
 
-    if i == 0:
-        joint_states = np.random.rand(1, 1, 6).astype(np.float32)
-        gripper_distance = np.random.rand(1, 1, 1).astype(np.float32)
-    else:
-        joint_states = predicted_action['joint_states'][:, next_state_index: next_state_index + 1, :]
-        gripper_distance = predicted_action['gripper_distance'][:,next_state_index: next_state_index + 1, :]
+    joint_states = np.random.rand(1, 1, 6).astype(np.float32)
+    gripper_distance = np.random.rand(1, 1, 1).astype(np.float32)
 
     # 构造 observation（需要根据你的数据格式调整）
     observation = {
@@ -46,29 +36,6 @@ for i in range(5):
             "annotation.human.action.task_description": [["pick and place task"]],
         }
     }
-
-    if i > 0:
-        # padding 到和原始 predicted_action 一样的 shape
-        original_joint_shape = predicted_action['joint_states'].shape
-        original_gripper_shape = predicted_action['gripper_distance'].shape
-
-        pad_joint = np.zeros(original_joint_shape, dtype=predicted_action['joint_states'].dtype)
-        pad_gripper = np.zeros(original_gripper_shape, dtype=predicted_action['gripper_distance'].dtype)
-
-        # 取 overlap，pad 到后面
-        valid_joint = predicted_action['joint_states'][:, pridict_horizon - inference_rtc_overlap_steps : pridict_horizon, :]
-        valid_gripper = predicted_action['gripper_distance'][:, pridict_horizon - inference_rtc_overlap_steps : pridict_horizon, :]
-
-        pad_joint[:, :inference_rtc_overlap_steps, :] = valid_joint
-        pad_gripper[:, :inference_rtc_overlap_steps, :] = valid_gripper
-
-        predicted_action['joint_states'] = pad_joint
-        predicted_action['gripper_distance'] = pad_gripper
-        
-        observation['action'] = {
-            "joint_states": predicted_action['joint_states'],
-            "gripper_distance": predicted_action['gripper_distance'],
-        }
 
     predicted_action, info = policy.get_action(observation)
     print(f"Step {i}: action[joint_states]=\n{predicted_action['joint_states']}")
